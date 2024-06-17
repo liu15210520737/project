a901f6887658e2956b79f9fdaaec9e0c9b268e7b
liu15210520737 <liu15210520737@qq.com>
2024-06-17 15:00:45 +0800

    update main.py

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from flask import *
 app = Flask(__name__)
 
 HOST,PORT = '0.0.0.0',8888
-index = '''
+
+index_html = '''
 
 <a>hello</a>
 '''
-login = '''
+login_html = '''
 <a>this is login page</a>
 '''
 
 @app.route("/")
 def index():
-  return index
+  return render_template_string(index_html)
 
 @app.route('/login')
 def login():
-  return login
+  return render_template_string(login_html)
 
 if __name__ == '__main__':
-  app.run(host=HOST, port=PORT)
+  app.run(host=HOST, port=PORT)           
