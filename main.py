8d2195ba7c10f9a24bff6d21a6e90535c7b13a7a
liu15210520737 <liu15210520737@qq.com>
2024-06-17 17:07:44 +0800

    update main.py

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from flask import *
 app = Flask(__name__)
 
 HOST,PORT = '0.0.0.0',8888
+DEBUG_MODE = True
 
 index_html = '''
-
+<html>
+<body>
 <a>hello</a>
+</body>
+</html>
 '''
 login_html = '''
+<html>
+<body>
 <a>this is login page</a>
+</body>
+</html>
 '''
 
 @app.route("/")
@@ -20,4 +28,4 @@ def login():
   return render_template_string(login_html)
 
 if __name__ == '__main__':
-  app.run(host=HOST, port=PORT)           
+  app.run(host=HOST, port=PORT, debug=DEBUG_MODE)
