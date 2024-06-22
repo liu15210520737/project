caca1bb039b073ef6cd4982543444b2cb6d2dec6
liu15210520737 <liu15210520737@qq.com>
2024-06-22 09:36:13 +0800

    update

--- panel_server.py
+++ panel_server.py
@@ -64,12 +64,50 @@ index_html = '''
 </html>
 '''
 login_html = '''
-<form action="/login" method="post">
-  <input type="text" name="username" placeholder="Username">
-  <input type="password" name="password" placeholder="Password">
-  <button type="submit">Submit</button>
-</form>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>login</title>
+		<link rel="stylesheet" type="text/css" href="/css/login_pageChange.css" />
+		<script src="/js/login_pageChange.js"></script>
+	</head>
+	<body>
+		<div class="control">
+			<div class="item">
+				<div class="active">登录</div><div>注册</div>
+			</div>
+			<div class="content">
+				<div style="display: block;">
+					<p>账号</p>
+					<input type="text" name='username' placeholder="Username" />
+					<p>密码</p>
+					<input type="password" name='password' placeholder="Password" />
+					<br/>
+					<input type="submit" value="登录" />
+				</div>
+				<div>
+					<p>用户名</p>
+					<input type="text" placeholder="username" />
+					<p>密码</p>
+					<input type="password" placeholder="password" />
+					<p>邮箱</p>
+					<input type="text" placeholder="email" />
+					<br/>
+					<input type="submit" value="登录" />
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
 '''
+#'''
+#<form action="/login" method="post">
+#  <input type="text" name="username" placeholder="Username">
+#  <input type="password" name="password" placeholder="Password">
+#  <button type="submit">Submit</button>
+#</form>
+#'''
 
 panel_html = '''
 null
@@ -79,6 +117,100 @@ error_404_html = '''
 error_500_html = '''
 '''
 error_html = '''
+'''
+login_pageChange_css = '''
+*{
+	margin: 0;
+	padding: 0;
+}
+body{
+	background: #f3f3f3;
+}
+.control{
+	width: 340px;
+	background: white;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%,-50%);
+	border-radius: 5px;
+}
+.item{
+	width: 340px;
+	height: 60px;
+	background: #eeeeee;
+}
+.item div{
+	width: 170px;
+	height: 60px;
+	display: inline-block;
+	color: black;
+	font-size: 18px;
+	text-align: center;
+	line-height: 60px;
+	cursor: pointer;
+}
+.content{
+	width: 100%;
+}
+.content div{
+	margin: 20px 30px;
+	display: none;
+	text-align: left;
+}
+p{
+	color: #4a4a4a;
+	margin-top: 30px;
+	margin-bottom: 6px;
+	font-size: 15px;
+}
+.content input[type="text"], .content input[type="password"]{
+	width: 100%;
+	height: 40px;
+	border-radius: 3px;
+	border: 1px solid #adadad;
+	padding: 0 10px;
+	box-sizing: border-box;
+}
+.content input[type="submit"]{
+	margin-top: 40px;
+	width: 100%;
+	height: 40px;
+	border-radius: 5px;
+	color: white;
+	border: 1px solid #adadad;
+	background: #00dd60;
+	cursor: pointer;
+	letter-spacing: 4px;
+	margin-bottom: 40px;
+}
+.active{
+	background: white;
+}
+.item div:hover{
+	background: #f6f6f6;
+}
+'''
+login_pageChange_js = '''
+window.onload = function(){
+	var item = document.getElementsByClassName("item");
+	var it = item[0].getElementsByTagName("div");
+	
+	var content = document.getElementsByClassName("content");
+	var con = content[0].getElementsByTagName("div");
+	
+	for(let i=0;i<it.length;i++){
+		it[i].onclick = function(){
+			for(let j=0;j<it.length;j++){
+				it[j].className = '';
+				con[j].style.display = "none";
+			}
+			this.className = "active";
+			it[i].index=i;
+			con[i].style.display = "block";
+		}
+	}
+}
 '''
 
 @app.route('/')
@@ -126,6 +258,17 @@ def error_404():
 @app.route('/500')
 def error_500():
   return render_template_string(error_500_html)
+
+@app.route('/css/<css>')
+def css(css):
+  if css == 'login_pageChange.css':
+    return render_template_string(login_pageChange_css)
+
+@app.route('/js/<js>')
+def js(js):
+  if js == 'login_pageChange.js':
+    return render_template_string(login_pageChange_js)
+
 if __name__ == '__main__':
   print('默认服务器端口号为8888')
   app.run(host=HOST, port=PORT, debug=DEBUG_MODE)
